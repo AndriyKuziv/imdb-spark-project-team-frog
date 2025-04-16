bc14ab7c1294c152b75ab98af960d7f1e154876b
AndriyKuziv <happyandreas18@gmail.com>
2025-04-17 02:01:44 +0300

    Updated existing queries, added new

--- andrii_queries.py
+++ andrii_queries.py
@@ -1,4 +1,5 @@
 from load_data import load_data
+from pyspark.sql import Window
 from pyspark.sql.functions import *
 
 data = load_data()
@@ -7,9 +8,9 @@ spark = data["spark"]
 dfs = {key: value for key, value in data.items() if key != "spark"}
 
 
-def getGermanRegionTitles():
-    print(f"-" * 21 + "Тайтли у німецькому регіоні" + "-" * 21)
-
+def getGermanRegionGenres():
+    print(f"-" * 21 + "Кількість тайтлів по жанрам у німецькому регіоні" + "-" * 21)
+    title_basics = dfs["title_basics"]
     title_akas = dfs["title_akas"]
 
     germanTitles = title_akas.filter(title_akas.region == "DE") \
@@ -20,8 +21,20 @@ def getGermanRegionTitles():
         col("language")
     )
 
-    germanTitles.show(20, truncate=False)
-    print("Кількість тайтлів у німецькому регіоні:", germanTitles.count())
+    df_split = title_basics.withColumn("genresArray", split(title_basics.genres, ","))
+
+    df_exploded = df_split.select(
+        df_split.tconst,
+        df_split.originalTitle,
+        explode(df_split.genresArray).alias("genre"),
+    )
+
+    germanTitles = germanTitles.join(df_exploded, germanTitles.titleId == df_exploded.tconst, how="inner")
+
+    genre_counts = germanTitles.groupBy("genre").count()
+
+    genre_counts.show(20, truncate=False)
+    print("Кількість різних жанрів у німецькому регіоні:", genre_counts.count())
 
 
 def getGermanMoviesFromYear(border_year):
@@ -53,32 +66,27 @@ def getWritersWithAvgRatingFrom(border_rating):
     print(f"-" * 21 + f"Сценаристи, у яких середній рейтинг робіт не менше {border_rating}" + "-" * 21)
 
     title_basics = dfs["title_basics"]
-    title_ratings = dfs["title_ratings"]
+    title_ratings = dfs["title_ratings"].filter(col("numVotes") >= 1000)
     title_crew = dfs["title_crew"]
     name_basics = dfs["name_basics"]
 
-    title_ratings = title_ratings.filter(title_ratings.numVotes >= 1000)
+    titles = title_basics.join(title_ratings, on="tconst", how="inner")
 
-    titles = title_basics.join(title_ratings, on="tconst", how="inner") \
-        .select(
-        title_basics["tconst"],
-        title_basics["titleType"],
-        title_basics["primaryTitle"],
-        title_basics["startYear"],
-        title_basics["genres"],
-        title_ratings["averageRating"],
-        title_ratings["numVotes"]
-    )
     writers = title_crew.join(name_basics, title_crew.writers == name_basics.nconst, how="inner") \
         .select(
-        title_crew["tconst"],
-        name_basics["nconst"],
-        name_basics["primaryName"].alias("writer")
-    )
+            title_crew["tconst"],
+            name_basics.primaryName.alias("writer")
+        )
 
-    avgWriterRating = titles.join(writers, on="tconst", how="inner") \
-        .groupBy("writer").agg(avg("averageRating").alias("avgWriterRating")) \
-        .filter(col("avgWriterRating") >= border_rating).orderBy(col("avgWriterRating"))
+    writer_window = Window.partitionBy("writer")
+    with_avg = titles.join(writers, on="tconst", how="inner") \
+        .withColumn("avgWriterRating", avg("averageRating").over(writer_window))
+
+    avgWriterRating = with_avg.filter(col("avgWriterRating") >= border_rating) \
+        .select(
+            with_avg["writer"],
+            with_avg["avgWriterRating"]
+        ).distinct().orderBy(col("avgWriterRating"))
 
     avgWriterRating.show(20, truncate=False)
     print(f"Кількість сценаристів, у яких середній рейтинг робіт не менше {border_rating}:", avgWriterRating.count())
@@ -95,10 +103,14 @@ def getCountriesLongestSeries():
         .join(title_akas, title_basics.tconst == title_akas.titleId, how="inner") \
         .filter(col("region") != "NULL")
 
-    episodesWithNumbers = episodes.join(title_episode, on="tconst", how="inner")
+    episodes_with_nums = episodes.join(title_episode, on="tconst", how="inner")
 
-    groupedByRegion = episodesWithNumbers.groupBy("region").agg(max("episodeNumber").alias("maxEpisodeNumber")) \
-        .orderBy(col("maxEpisodeNumber"), ascending=False)
+    region_window = Window.partitionBy("region")
+
+    with_max = episodes_with_nums.withColumn("maxEpisodeNumber", max("episodeNumber").over(region_window))
+
+    groupedByRegion = with_max.select("region", "maxEpisodeNumber") \
+        .distinct().orderBy(col("maxEpisodeNumber").desc())
 
     groupedByRegion.show(20, truncate=False)
     print(f"Кількість регіонів, в яких виходив хоча б один серіал:", groupedByRegion.count())
@@ -117,12 +129,12 @@ def getEpisodesWithRatingFrom(border_rating):
     filteredEpisodes = title_basics.join(title_ratings, on="tconst", how="inner") \
         .filter(title_ratings.averageRating >= border_rating).orderBy(col("averageRating")) \
         .select(
-        col("primaryTitle").alias("episodeTitle"),
-        col("startYear").alias("releaseYear"),
-        col("genres"),
-        col("averageRating"),
-        col("numVotes"),
-    )
+            col("primaryTitle").alias("episodeTitle"),
+            col("startYear").alias("releaseYear"),
+            col("genres"),
+            col("averageRating"),
+            col("numVotes"),
+        )
 
     filteredEpisodes.show(20, truncate=False)
     print(f"Кількість епізодів, в яких середній рейтинг не менше {border_rating}:", filteredEpisodes.count())
@@ -160,10 +172,25 @@ def getTitlesWithSameDirectorWriter():
     print("Кількість тайтлів, у яких одна людина виконує роль сценариста та режисера:", titles.count())
 
 
+def getAvgTitleTypeRuntime():
+    print(f"-" * 21 + "Середня тривалість тайтлу по його типу" + "-" * 21)
+    title_basics = dfs["title_basics"]
+
+    grouped = title_basics.filter(title_basics.runtimeMinutes.isNotNull()) \
+        .groupBy(title_basics.titleType).agg(avg(title_basics.runtimeMinutes).alias("averageRuntime"))
+
+    grouped.show(20, truncate=False)
+    print("Кількість типів тайтлів:", grouped.count())
+
+
 def run_andrii_queries():
-    getGermanRegionTitles()
+    getGermanRegionGenres()
+    getAvgTitleTypeRuntime()
     getGermanMoviesFromYear(2010)
     getTitlesWithSameDirectorWriter()
     getCountriesLongestSeries()
     getEpisodesWithRatingFrom(8.1)
     getWritersWithAvgRatingFrom(8.1)
+
+
+getAvgTitleTypeRuntime()
